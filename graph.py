62b861489a54595bb4d93803090c0166fda91365
ilikecubesnstuff <subho1618@gmail.com>
2018-11-29 14:38:14 +0400

    Added stuff (except it doesn't actually work fml).

--- graph.py
+++ graph.py
@@ -1,14 +1,28 @@
 # Imports
 import os
+from random import seed, random
+from math import sqrt
+# import pygame
+# from pygame.locals import *
+
+distance = lambda x, y: sqrt((x[0] - y[0])**2 + (x[1] - y[1])**2)
 
 class Graph:
 
-    distance = lambda x, y: sqrt(x**2 + y**2)
+    def generate_random_graph(self, n, **kwargs):
+        try:
+            seed(kwargs["seed"])
+        except:
+            print("CAUTION: No seed used for random graph generation. (Graph data may be lost)")
+        self.graph = [[random(), random()] for i in range(n)]
+        self.d_matrix = [[distance(i, j) for j in self.graph] for i in self.graph]
 
-    def __init__(self):
-        pass
+    def path_length(self, path):
+        return sum(self.d_matrix[path[i], path[i + 1]] for i in range(-1, len(path) - 2))
 
 _inst = Graph()
+generate_random_graph = _inst.generate_random_graph
+path_length = _inst.path_length
 
 if hasattr(os, "fork"):
     os.register_at_fork(after_in_child = _inst.start)
