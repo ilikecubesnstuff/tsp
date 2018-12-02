9cfeb499d4eaa6e2a8cfaa89280c17e99de0ea57
ilikecubesnstuff <subho1618@gmail.com>
2018-12-03 03:35:08 +0400

    Added display functionality (pygame) and tweaked brute force alg.

--- brute_force.py
+++ brute_force.py
@@ -1,5 +1,6 @@
 from itertools import permutations
 import graph
+import time
 
 def brute_force(display=False):
 
@@ -14,15 +15,29 @@ def brute_force(display=False):
             continue
 
         # measures length
-        length = graph.path_length(list(path) + [n - 1])
+        path = list(path) + [n - 1]
+        length = graph.path_length(path)
+        # print(path, length)
 
         # check / replace
         if length < best_path_length:
             best_path = path
             best_path_length = length
 
-    return graph.path_length(best_path)
+        # display
+        graph.display_refresh()
+        graph.display_path(best_path, (0,255,0))
+        graph.display_path(path, (255,255,255))
+        graph.display_update()
+        # time.sleep(2)
+
+    graph.display_refresh()
+    graph.display_path(best_path, (0,255,0))
+    graph.display_update()
+    return best_path
 
 if __name__ == "__main__":
-    p = brute_force(graph.generate_random_graph(10, display=True))
-    print(p)
+    graph.generate_random_graph(9, display=True, seed=3198)
+    p = brute_force(display=True)
+    print(p, graph.path_length(p))
+    graph.display_stick()
--- graph.py
+++ graph.py
@@ -2,52 +2,65 @@
 import os
 from random import seed, random
 from math import sqrt
-# import pygame
-# from pygame.locals import *
+import pygame
+from pygame.locals import *
 
 distance = lambda x, y: sqrt((x[0] - y[0])**2 + (x[1] - y[1])**2)
 
 class Graph:
 
-    # Graph Methods
+    # Graph methods
 
-    def generate_random_graph(self, n, **kwargs):
+    def generate_random_graph(self, n, display=False, **kwargs):
+
         try:
             seed(kwargs["seed"])
         except:
             print("CAUTION: No seed used for random graph generation. (Graph data may be lost)")
+
         self.vertices = [[random(), random()] for i in range(n)]
         self.d_matrix = [[distance(i, j) for j in self.vertices] for i in self.vertices]
+
+        if display:
+            pygame.init()
+            self.screen = pygame.display.set_mode((500,500))
+            pygame.display.set_caption("TSP - %s node graph"%(self.size()))
 
     def size(self):
         return len(self.vertices)
 
     def path_length(self, path):
-        return sum(self.d_matrix[path[i]][path[i + 1]] for i in range(-1, len(path) - 2))
+        return sum(self.d_matrix[path[i]][path[i + 1]] for i in range(-1, len(path) - 1))
 
-    # Display Methods
+    # Display methods
 
     def display_refresh(self):
-        # make screen blank
-        pass
+        self.screen.fill((0,0,0))
+        for vertex in self.vertices:
+            pygame.draw.circle(self.screen, (255, 255, 255), (int(vertex[0]*500), int(vertex[1]*500)), 5)
 
-    def display_path(self):
-        # draw path by connecting dots
-        # pygame.draw.lines
-        pass
+    def display_path(self, path, color, loop=True):
+        pygame.draw.lines(self.screen, color, loop, [(int(self.vertices[i][0]*500), int(self.vertices[i][1]*500)) for i in path])
 
     def display_update(self):
-        # pygame.display.flip()
-        pass
+        pygame.display.flip()
 
     def display_stick(self):
-        # insert game loop
-        pass
+        while 1:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    exit()
 
 _inst = Graph()
+# Graph methods
 generate_random_graph = _inst.generate_random_graph
 size = _inst.size
 path_length = _inst.path_length
+# Display methods
+display_refresh = _inst.display_refresh
+display_path = _inst.display_path
+display_update = _inst.display_update
+display_stick = _inst.display_stick
 
 if hasattr(os, "fork"):
     os.register_at_fork(after_in_child = _inst.start)
